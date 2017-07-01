16001be46fda040be28dba1d71402e6b331e42b3
Dave Gray <davegray@Daves-MacBook-Air.local>
2017-07-01 10:19:23 -0600

    Ugh, this map API

--- js/searchtest.js
+++ js/searchtest.js
@@ -9,10 +9,11 @@ var citystate;
 var citystateNoSpaces;
 var geocoder;
 var breweryDots = [];
+var slicedBreweryDots = [];
 var addressdots;
-// var queryURL = "http://beermapping.com/webservice/loccity/1e07e953394846ca559ab1d498fb5b41/" + "newyork,ny" + "&s=json";
-// var geocode = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + citystate + '&key=AIzaSyCzv6_hzw87dgf9atP_8y6Xm82jfAjDUaA';
 
+
+//Center the map over Denver
   function initMap() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(39.7538126, -104.9907165);
@@ -23,14 +24,15 @@ var addressdots;
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
- $("#add-search-btn").on("click", function() {
+
+  //On search, use beer mapping API to find breweries close to City and State selected
+ $(document).on("click", "#add-search-btn", function() {
   event.preventDefault();
 
   citystate = $("#search-input").val().trim();
   citystateNoSpaces = citystate.replace(/\s+/g, '');
-  // console.log(citystateNoSpaces);
-
-
+  
+      //AJAX call to the Beer Mapping API
       var queryURL = "http://beermapping.com/webservice/loccity/1e07e953394846ca559ab1d498fb5b41/" + citystateNoSpaces + "&s=json";
         $.ajax({
           url: queryURL,
@@ -39,96 +41,59 @@ var addressdots;
         .done(function(response) {
           // console.log(response);
 
+          //Loop through responses and return address infomration of breweries-- may just need name
           for (var i = 0; i < response.length; i++) {
             currentBrewery = response[i];
-            address = {
-              // name: currentBrewery.name,
-              street: currentBrewery.street,
-              city: currentBrewery.city,
-              state: currentBrewery.state,
-              zip: currentBrewery.zip,
-                }
+            // address = {
+            //   // name: currentBrewery.name,
+            //   street: currentBrewery.street,
+            //   city: currentBrewery.city,
+            //   state: currentBrewery.state,
+            //   zip: currentBrewery.zip,
+            //     }
             status = currentBrewery.status;
 
             if (status === "Brewery") { 
-            // console.log(currentBrewery.name);
             // console.log(address);
             
-            // addressdots = JSON.stringify(currentBrewery.name);
             breweryDots.push(currentBrewery.name);
+
+            slicedBreweryDots = breweryDots.slice(0,9);
+            console.log(slicedBreweryDots);
             
-            console.log(breweryDots);
-            // getDots();
+            // console.log(breweryDots);
                
             }       
           }
+          findBrews();
         });
 
-  codeAddress();
 
-   function codeAddress() {
-    var location = document.getElementById('search-input').value;
-    geocoder.geocode( { address: citystate}, function(results, status) {
+    //Call function to create pins for each of the breweries returned
+    
+    function findBrews() {
+    for (var i = 0; i < slicedBreweryDots.length; i++) {
+    makePins(slicedBreweryDots[i]);
+    }
+  }
+
+    function makePins(a) {
+    geocoder.geocode( { address: a}, function(results, status) {
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
-
-  infoWindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-  map.addListener('idle', performSearch);
-//   } 
-// }
-
-  function performSearch() {
-
-   for (i = 0; i < breweryDots.length; i++) {
-    var request = {
-      bounds: map.getBounds(),
-      keyword: breweryDots[i]
-    };
-  }
-    service.radarSearch(request, callback);
-  }
-
-  function callback(results, status) {
-    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-      console.error(status);
-      return;
+          }
+      });
     }
-    for (var i = 0, result; result = results[i]; i++) {
-    addMarker(result);
-  }
-}
-
-function addMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-    icon: {
-      url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
-      anchor: new google.maps.Point(10, 10),
-      scaledSize: new google.maps.Size(10, 17)
-    }
-  });
-
-
-  google.maps.event.addListener(marker, 'click', function() {
-    service.getDetails(place, function(result, status) {
-      if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.error(status);
-        return;
-      }
-      infoWindow.setContent(result.name);
-      infoWindow.open(map, marker);
-    });
-  });
-}
 
 });
+
+  
 
 
 
